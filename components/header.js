a3b4568047f51e4051ff3f26abc32d3eb77031c4
Moyrasc <noemisolac@gmail.com>
2022-10-21 16:48:28 +0200

    style component header

--- components/header.js
+++ components/header.js
@@ -1,13 +1,14 @@
 import Image from "next/future/image";
 import Link from "next/link";
-import logo from "../public/img/logo.svg"
+import styles from "../styles/header.module.css"
+
 
 const Header = () => {
     return ( 
-    <header>
-        <div className="container">
+    <header className={styles.header}>
+        <div className={`container ${styles.navigation}`}>
             <Image src="/img/logo.svg" alt="logo "width={300} height={40}/>
-            <nav>
+            <nav className={styles.nav}>
                 <Link href="/"> Inicio </Link>
                 <Link href="/about"> Nosotros </Link>
                 <Link href="/blog"> Blog </Link>
