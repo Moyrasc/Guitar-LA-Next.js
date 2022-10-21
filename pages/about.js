de7fd38713fddc58c28699a13c2283d7b5f3cf59
Moyrasc <noemisolac@gmail.com>
2022-10-21 12:43:52 +0200

    create component about

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import React from "react";
+
+const About = () => {
+    return ( 
+        <Link href="/" >Inicio </Link>
+    );
+}
+
+export default About;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
-
+import Link from "next/link"
 
 export default function Home() {
   return (
     <>
     <h1>Hola mundo next</h1>
+    <Link href="/about">Nosotros</Link>
     </> 
   )
 }
