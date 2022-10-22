58317a19ddcaf73bf65aaf42f342ff484454c9d4
Moyrasc <noemisolac@gmail.com>
2022-10-22 12:09:19 +0200

    style footer

--- components/footer.js
+++ components/footer.js
@@ -1,8 +1,19 @@
+import Link from "next/link";
+import styles from "../styles/footer.module.css";
+
 const Footer = () => {
     return ( 
-        <>
-        <p>Footer</p>
-        </>
+        <footer className={styles.footer}>
+            <div className={`container ${styles.content} `}>
+                <nav className={styles.nav}>
+                    <Link href="/"> Inicio </Link>
+                    <Link href="/about"> Nosotros </Link>
+                    <Link href="/blog"> Blog </Link>
+                    <Link href="/store"> Tienda </Link>
+                </nav>
+                <p className="copyright">&copy; Todos los derechos reservados {new Date().getFullYear()}</p>
+            </div>
+        </footer>
     );
 }
 export default Footer;
