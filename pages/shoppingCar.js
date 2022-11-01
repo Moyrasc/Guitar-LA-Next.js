9dc98a87a2a8d330005835ba2f161cb23cbede90
Moyrasc <noemisolac@gmail.com>
2022-11-01 12:51:18 +0100

    add styles shopping car and logic component

--- pages/shoppingCar.js
+++ pages/shoppingCar.js
@@ -1,7 +1,8 @@
 import Layout from "../components/layout.js" 
 import styles from "../styles/shoppingCar.module.css"
+import Image from "next/future/image.js";
 
-const ShoppingCar = () => {
+const ShoppingCar = ({shoppingCar}) => {
     return ( 
         <>
         <Layout
@@ -11,6 +12,21 @@ const ShoppingCar = () => {
                 <div className={styles.content}>
                     <div className={styles.shopping}>
                         <h2>Artículos</h2>
+                        {shoppingCar.length === 0 ? "Cesta vacía" : shoppingCar.map(product => {
+
+                            <div key={product.id} className={styles.product}>
+                                <div>
+                                    <Image width={250} height={480} src={product.image} alt={product.name}/>
+
+                                </div>
+                                <div>
+                                    <p className={styles.name}> {product.name} </p>
+                                    <p className={styles.precio}> $ <span>{product.price}</span></p>
+                                    <p className={styles.subtotal}> Subtotal: $<span> {product.amount * product.price}</span></p>
+                                </div>
+
+                            </div>
+                        })}
                     </div>
                     <aside className={styles.summary}>
                         <h3> Resumen del Pedido </h3>
