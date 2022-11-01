26b9df5d753eda25b731551ac59cebaa6d1be1ae
Moyrasc <noemisolac@gmail.com>
2022-11-01 17:03:39 +0100

    add select amount guitar

--- pages/shoppingCar.js
+++ pages/shoppingCar.js
@@ -3,16 +3,15 @@ import styles from "../styles/shoppingCar.module.css"
 import Image from "next/future/image.js";
 
 const ShoppingCar = ({shoppingCar}) => {
-    return ( 
-        <>
+    return (       
         <Layout
         title="Carrito compras">
             <main className="container">
                 <h1 className="heading"> Carrito </h1>
-                <div className={styles.content}>
+                <div className={styles.contents}>
                     <div className={styles.shopping}>
                         <h2>Artículos</h2>
-                        {shoppingCar.length === 0 ? "Cesta vacía" : shoppingCar.map(product => {
+                        {shoppingCar.length === 0 ? "Cesta vacía" : (shoppingCar.map(product => (
 
                             <div key={product.id} className={styles.product}>
                                 <div>
@@ -21,12 +20,21 @@ const ShoppingCar = ({shoppingCar}) => {
                                 </div>
                                 <div>
                                     <p className={styles.name}> {product.name} </p>
+                                    <div className={styles.amount}>
+                                        <p>Cantidad: </p>
+                                        <select className={styles.select}>
+                                            <option value="1"> 1 </option>
+                                            <option value="2"> 2 </option>
+                                            <option value="3"> 3 </option>
+                                            <option value="4"> 4 </option>
+                                        </select>
+                                    </div>
                                     <p className={styles.precio}> $ <span>{product.price}</span></p>
                                     <p className={styles.subtotal}> Subtotal: $<span> {product.amount * product.price}</span></p>
                                 </div>
 
                             </div>
-                        })}
+                        )))}
                     </div>
                     <aside className={styles.summary}>
                         <h3> Resumen del Pedido </h3>
@@ -35,7 +43,7 @@ const ShoppingCar = ({shoppingCar}) => {
                 </div>
             </main>
         </Layout>
-        </>
+       
     );
 }
 
